c705a3611ce4bb3ef4e739cc9522d089f28a1180
amitvrepo <mailforamitv@gmail.com>
2025-04-19 19:25:21 +0530

    spring boot sample-web app for  Azure AKS .

---
+++ sample-web/src/main/java/com/example/sampleweb/controller/HelloController.java
@@ -0,0 +1,20 @@
+/*
+ * package com.example.sampleweb.controller;
+ * 
+ * //import org.springframework.ui.Model; //import
+ * org.springframework.web.bind.annotation.GetMapping; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * @RestController
+ * 
+ * @RequestMapping("/hello") public class HelloController {
+ * 
+ * 
+ * 
+ * public String hello(Model model) { model.addAttribute("message",
+ * "Hello, Azure Kubernetes!"); return "hello"; }
+ * 
+ * @GetMapping("/hello") public String sayHello() { return
+ * "Hello, Azure Kubernetes!"; } }
+ */
